07ec7199d39816bf9b07bd63d925af18b45ec54b
Odas <Odas@DESKTOP-CB8QNTM>
2022-06-06 12:20:06 +0200

    Quiz Prototyp
    Damit ihr mal fürs erste weitermachen könnt, ihr könnt dann noch was
    hinzufügen wenn was fehlt

--- Spengerkarten/src/Quiz.java
+++ Spengerkarten/src/Quiz.java
@@ -1,4 +1,46 @@
+import java.util.HashMap;
 
 public class Quiz {
-	//Hier ist die HashMap und so drinnen (Technik hitner dem Ganzen)
+	
+	//Hier ist die HashMap und so drinnen (Technik hinter dem Ganzen)
+	
+	// Name vom Quiz
+	private String quizName;
+	
+	// values (zb vokabeln)
+	private HashMap<String, String> values;
+	
+	public Quiz(String quizName) {
+		values = new HashMap<>();
+		this.quizName = quizName;
+	}
+	
+	public String getName() {
+		return quizName;
+	}
+	
+	// vokabel hinzufügen
+	public void addVocab(String key, String value) {
+		values.put(key, value);
+	}
+	
+	public String getValue(String key) {
+		return values.get(key);
+	}
+	
+	public int getSize() {
+		return values.size();
+	}
+	
+	// schaut ob key bzw value ein Pfad ist
+	public boolean isPath(String s) {
+		if(s.contains("\\")||s.contains("/")) return true;
+		return false;
+	}
+	
 }
+
+
+
+
+
