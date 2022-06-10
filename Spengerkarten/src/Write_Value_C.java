9e49fc65d71b845020a713c6f24e4ef6c568f015
Christoph456 <christoph260@icloud.com>
2022-06-11 00:34:54 +0200

    Bissl was geändert 3.0

---
+++ Spengerkarten/src/Write_Value_C.java
@@ -0,0 +1,26 @@
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Write_Value_C {
+	Quiz newQuiz;
+	int index = 0;
+	BorderPane bp = new BorderPane();
+	
+	public void start(Stage stage) throws Exception
+	{
+		System.out.println("Fortnite");
+		
+		
+	}
+	
+	public void quizUebernehmen(Stage stage, Quiz quiz) {
+		newQuiz = quiz;
+		index = 0;
+		try {
+			start(stage);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
